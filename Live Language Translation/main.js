b1de391c22bd26c54cb75b5e8f72e025757d5636
Amit-keshri <95470883+Amit-keshri@users.noreply.github.com>
2023-07-31 18:14:48 +0530

    Update main.js

--- Live Language Translation/main.js
+++ Live Language Translation/main.js
@@ -7,7 +7,6 @@ let cpyBtn = document.querySelector('bx-copy');
 let countValue = document.querySelector('.code_length');
 let exchangLang = document.querySelector('.bx-transfer');
 
-// langOption,array.forEach((get, con) => {
     langOption.forEach((get, con) => {
     for(let countryCode in language){
         let selected;
@@ -29,38 +28,38 @@ fromText.addEventListener('input', function(){
     let transLINK = `https://api.mymemory.translated.net/get?q=${content}!&langpair=${fromContent}|${transContent}`;
 
     fetch(transLINK).then(translate => translate.json()).then(data =>{
-        // transText.value = data.responseData.translatedText;
-        console.log(data);
+        transText.value = data.responseData.translatedText;
+        // console.log(data);
     })
 })
-// fromVoice.addEventListener('click', function(){
-//     let fromTalk;
-//     fromTalk = new SpeechSynthesisUtterance(fromText.value);
-//     fromTalk.lang = langOption[0].value;
-//     speechSynthesis.speak(fromTalk)
-// })
+fromVoice.addEventListener('click', function(){
+    let fromTalk;
+    fromTalk = new SpeechSynthesisUtterance(fromText.value);
+    fromTalk.lang = langOption[0].value;
+    speechSynthesis.speak(fromTalk)
+})
 
-// toVoice.addEventListener('click', function () {
-//     let fromTalk;
-//     fromTalk = new SpeechSynthesisUtterance(transText.value);
-//     fromTalk.lang = langOption[1].value;
-//     speechSynthesis.speak(fromTalk)
-// })
+toVoice.addEventListener('click', function () {
+    let fromTalk;
+    fromTalk = new SpeechSynthesisUtterance(transText.value);
+    fromTalk.lang = langOption[1].value;
+    speechSynthesis.speak(fromTalk)
+})
 
-// // cpyBtn.addEventListener('click',function(){
-// //     navigator.clipboard.writeText(transText.value);
-// // })
+cpyBtn.addEventListener('click',function(){
+    navigator.clipboard.writeText(transText.value);
+})
 
-// fromText.addEventListener('keyup',function(){
-//     countValue.innerHTML = `${fromText.value.length}/5,000`;
-// })
+fromText.addEventListener('keyup',function(){
+    countValue.innerHTML = `${fromText.value.length}/5,000`;
+})
 
-// exchangLang.addEventListener('click',function(){
-//     let tempText = fromText.value;
-//     fromText.value = transText.value;
-//     transText.value = tempText;
+exchangLang.addEventListener('click',function(){
+    let tempText = fromText.value;
+    fromText.value = transText.value;
+    transText.value = tempText;
 
-//     let tempOpt = langOption[0].value;
-//     langOption[0].value = langOption[1].value;
-//     langOption[1].value = tempOpt;
-// })
+    let tempOpt = langOption[0].value;
+    langOption[0].value = langOption[1].value;
+    langOption[1].value = tempOpt;
+})
